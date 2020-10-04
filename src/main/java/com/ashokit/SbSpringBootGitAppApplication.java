77f6520eeeda4b73e70b9e1b4af9403369fe6d1c
swapnilthakre468 <tswapnil468@gmail.com>
2020-10-04 23:36:58 +0530

    main class modified

--- src/main/java/com/ashokit/SbSpringBootGitAppApplication.java
+++ src/main/java/com/ashokit/SbSpringBootGitAppApplication.java
@@ -2,12 +2,14 @@ package com.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SbSpringBootGitAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbSpringBootGitAppApplication.class, args);
+		ConfigurableApplicationContext applicationContext=SpringApplication.run(SbSpringBootGitAppApplication.class, args);
+	    applicationContext.close();
 	}
 
 }
