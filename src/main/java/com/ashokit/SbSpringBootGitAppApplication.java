344ac394cf83c09d569bc5d6d418ece311035fe8
swapnilthakre468 <tswapnil468@gmail.com>
2020-10-04 23:23:03 +0530

    initial commit

---
+++ src/main/java/com/ashokit/SbSpringBootGitAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSpringBootGitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSpringBootGitAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashokit/SbSpringBootGitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSpringBootGitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
